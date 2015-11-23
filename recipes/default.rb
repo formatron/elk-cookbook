3e7b2578f1cfa4e9ee2b8365f278ec28c7da3142
Peter Halliday <pghalliday@gmail.com>
2015-11-23 14:12:51 +0100

    limit access to elasticsearch

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+node.override['formatron_elasticsearch']['params']['network.host'] = 'localhost'
+
 include_recipe 'formatron_elasticsearch::default'
 include_recipe 'formatron_logstash::default'
 include_recipe 'formatron_kibana::default'
